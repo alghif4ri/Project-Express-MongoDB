42e5889aa1b3431462090e9cbb8261d5561b22f1
LuckyNoobz <r.abudzar@gmail.com>
2024-02-23 20:27:04 +0700

    model and seed for data dummy

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
 
+// MODELS
+const Product = require("./models/product");
+
 mongoose
   .connect("mongodb://127.0.0.1/shop_db")
   .then((result) => {
@@ -16,9 +19,9 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res)=> {
-    res.send('Hallo!')
-})
+app.get("/", (req, res) => {
+  res.send("Hallo!");
+});
 
 app.listen(port, () => {
   console.log(`ShopApp is running on http://localhost:${port}`);
---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  color: { type: String, required: true },
+  size: { type: String, enum: ["S", "M", "L", "XL", "XXL"], required: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
