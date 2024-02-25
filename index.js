c676e3aa0789859a99bb62decb552751b13956c6
LuckyNoobz <r.abudzar@gmail.com>
2024-02-25 22:26:20 +0700

    Error Handling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const app = express();
+const ErrorHandler = require("./ErrorHandler");
 const port = 3001;
 
 // MODELS
@@ -29,14 +30,15 @@ app.get("/products", async (req, res) => {
   const { category } = req.query;
   if (category) {
     const products = await Product.find({ category });
-    res.render("products/index", { products , category});
+    res.render("products/index", { products, category });
   } else {
     const products = await Product.find({});
-    res.render("products/index", { products, category: 'All' });
+    res.render("products/index", { products, category: "All" });
   }
 });
 
 app.get("/products/create", (req, res) => {
+  // throw new ErrorHandler("this custom error", 503);
   res.render("products/create");
 });
 
@@ -45,29 +47,50 @@ app.post("/products", async (req, res) => {
   await product.save();
   res.redirect(`/products/${product._id}`);
 });
-app.get("/products/:id", async (req, res) => {
-  // const {id} = req.params
-  const product = await Product.findById(req.params.id);
-  res.render("products/show", { product });
+app.get("/products/:id", async (req, res, next) => {
+  try {
+    // const {id} = req.params
+    const product = await Product.findById(req.params.id);
+    res.render("products/show", { product });
+  } catch (error) {
+    return next(new ErrorHandler("Product Not Found", 404));
+  }
 });
-app.get("/products/:id/edit", async (req, res) => {
+app.get("/products/:id/edit", async (req, res, next) => {
   // const {id} = req.params
-  const product = await Product.findById(req.params.id);
-  res.render("products/edit", { product });
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("products/edit", { product });
+  } catch (error) {
+    return next(new ErrorHandler("Product Not Found", 404));
+  }
 });
 
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/products/${product._id}`);
+app.put("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/products/${product._id}`);
+  } catch (error) {
+    return next(new ErrorHandler("Invalid Data", 422));
+  }
 });
 
-app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndDelete(id);
-  res.redirect("/products");
+app.delete("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+  } catch (error) {
+    return next(new ErrorHandler("Error Deleting Product", 500));
+  }
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "something went wrong" } = err;
+  res.status(status).send({ message });
 });
 
 app.listen(port, () => {
