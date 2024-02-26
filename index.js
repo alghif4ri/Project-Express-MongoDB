8615a558233f25a3d2c1d009e10ce68c7763c47d
LuckyNoobz <r.abudzar@gmail.com>
2024-02-26 16:03:20 +0700

    fix error handler | wrapAsync

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
 app.get("/", (req, res) => {
   res.send("Hallo!");
 });
@@ -42,55 +47,65 @@ app.get("/products/create", (req, res) => {
   res.render("products/create");
 });
 
-app.post("/products", async (req, res) => {
+app.post("/products", wrapAsync(async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.redirect(`/products/${product._id}`);
-});
-app.get("/products/:id", async (req, res, next) => {
-  try {
-    // const {id} = req.params
-    const product = await Product.findById(req.params.id);
+}));
+
+app.get(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
     res.render("products/show", { product });
-  } catch (error) {
-    return next(new ErrorHandler("Product Not Found", 404));
-  }
-});
-app.get("/products/:id/edit", async (req, res, next) => {
-  // const {id} = req.params
-  try {
+  })
+);
+app.get(
+  "/products/:id/edit",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(req.params.id);
     res.render("products/edit", { product });
-  } catch (error) {
-    return next(new ErrorHandler("Product Not Found", 404));
-  }
-});
+  })
+);
 
-app.put("/products/:id", async (req, res, next) => {
-  try {
+app.put(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {
       runValidators: true,
     });
     res.redirect(`/products/${product._id}`);
-  } catch (error) {
-    return next(new ErrorHandler("Invalid Data", 422));
-  }
-});
+  })
+);
 
-app.delete("/products/:id", async (req, res, next) => {
-  try {
+app.delete(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.redirect("/products");
-  } catch (error) {
-    return next(new ErrorHandler("Error Deleting Product", 500));
-  }
+  })
+);
+
+const validatorHandler = (err) => {
+  err.status = 400;
+  err.message = Object.values(err.errors).map((item) => item.message);
+  return new ErrorHandler(err.message, err.status);
+};
+
+app.use((err, req, res, next) => {
+  console.dir(err);
+  if (err.name === "ValidationError") err = validatorHandler(err);
+  if (err.name === "CastError") err = CastError(err);
+  next(err);
 });
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "something went wrong" } = err;
-  res.status(status).send({ message });
+  res.status(status).send(message);
 });
 
 app.listen(port, () => {
